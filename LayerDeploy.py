47f7f7d09959a48273c6f3818e7981cda5f42e58
Rathva Kaushikkumar <kaushikkumar@iisc.ac.in>
2025-03-19 17:16:02 +0530

    appling LayerDeploy

--- LayerDeploy.py
+++ LayerDeploy.py
@@ -1,2 +1,20 @@
 # Import python file with model implementted.
+import multiprocessing
+from ModelProfiling import *
+# import multiprocessing
 
+class Deployer:
+    def __init__(self, layers):
+        self.pipes = {}
+        self.layers = layers
+        self.processes = []
+        for layer in layers:
+            self.pipes[layer] =multiprocessing.Pipe()
+    def create_processes(self,):
+        prev_layers = []
+        for layer in self.layers:
+            p = multiprocessing.Process(target=layer, args=(self.pipes[layer], self.pipes[prev_layers[0]]))
+            prev_layers = [layer]
+            self.processes.append(p)
+            p.start()
+            
