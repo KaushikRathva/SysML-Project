dd01337a9ff7323d901be41beb1e77d6833ed83c
GayatriMadduri <maddurigayatrii@gmail.com>
2025-03-20 17:08:14 +0530

    Comments in layerprofiling

--- LayerMapping.py
+++ LayerMapping.py
@@ -19,7 +19,8 @@ def exhaustive_search(cur_block_idx,occupied_cores,block_core_map,cores_per_bloc
             cur_block_core_map[cur_block_idx] = cur_core
             exhaustive_search(cur_block_idx+1,occupied_cores,block_core_map,cores_per_block,total_blocks,cur_block_core_map)
 
-#Calculation of communication time
+#Calculation of communication time after exhaustive search
+#Input: List of dictionaries with keys = block id, value = core id, c2clatency 
 def calc_communication(block_core_map,c2cLatency,modelTrafficProfile):
     for cur_map in block_core_map:
         for block1 in cur_map:
@@ -30,23 +31,35 @@ def calc_communication(block_core_map,c2cLatency,modelTrafficProfile):
     return np.array(comm_time)
 
 #Approximate modelTrafficProfile is highest between consecutive layers
+#Input is c2cLatency and computeblocks from cpuprofiling
+#Output is block to core mapping. Dictionary with keys = block index, value = core indec
 def heuristic1(c2clatency,computeBlocks):
     c2clatency = np.array(c2clatency)
     block_core_map={}
     least_latency_cores = []
+    least_latency_cores_idx = {}
     min_latency_per_core = np.argmin(c2clatency,axis = 1)
     second_min_latency_per_core = np.argmin(c2clatency[c2clatency != np.min(c2clatency,axis = 1)],axis = 1)
     num_blocks = len(computeBlocks)
 
     for start_idx in range(c2clatency.shape[0]):
-        cur_idx 
+        cur_idx = start_idx
         for idx in range(num_blocks):
-            least_latency_cores[idx] = 
+            min_latency = min_latency_per_core[cur_idx]
+            if min_latency in least_latency_cores_idx:
+                min_latency = second_min_latency_per_core[cur_idx]
+            cur_idx = min_latency
+            least_latency_cores_idx.add(cur_idx)
+            least_latency_cores.append(min_latency)
+    
     for block_idx,_ in enumerate(computeBlocks):
         block_core_map[block_idx] = least_latency_cores[block_idx]
     return block_core_map
 
 # Layer mapping
+#Primary function: Input is search mode. Can be exhaustive or heuristic
+#Output is leastcostmap. A function that provides best mapping from block to core. 
+#Keys are block indices and values are core indices
 def layerMapping(searchMode = "exhaustive"):
     
     #Get information of model profiling and CPU profiling
@@ -81,7 +94,7 @@ def layerMapping(searchMode = "exhaustive"):
 
     #Heuristic Search Algorithm
     if(searchMode == "heuristic"):
-
+        least_cost_map = heuristic1(c2cLatency,computeBlocks)
         
 
         
